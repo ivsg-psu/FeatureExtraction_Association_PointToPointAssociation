33d14240ace5e575a1da648c79860aa4285c957c
Shashank Garikipati <shashank7g@gmail.com>
2022-07-01 13:59:30 +0530

    FEAT: cleaned up comments and added sections

--- Functions/script_test_fcn_Points_fillPointSetViaUserInputs.m
+++ Functions/script_test_fcn_Points_fillPointSetViaUserInputs.m
@@ -5,57 +5,52 @@
 % script_test_fcn_Path_fillPathViaUserInputs.m
 % Questions or comments? sbrennan@psu.edu 
 
-% Revisions
-%     2022_01_28
-%     -- First write of the code
+%% Revisions History
+% 2022_01_28
+% -- First write of the code
+% 2022_07_1
+% -- cleaned up comments and organized sections
 
-close all;
-clc
+clc, close all;
+%% BASIC example
+% Shows step by step instructions about using function
 
+fig_num = 1;
+h = figure(fig_num);
+hold on;
 
-%% BASIC example 1
-if 1==1  % Intentionally comment this out so that doesn't autorun. Forces user to read the script!
-    
-    %% Code starts here
-    fig_num = 1;
-    h = figure(fig_num);
-    hold on;
-    
-    num_iterations = input('How many point sets do you want to create? [Hit enter for default of 3]:','s');
-    if isempty(num_iterations)
-        num_iterations = 3;
-    else
-        num_iterations = str2double(num_iterations);
-    end
-    fprintf(1,'\n Filling in %.0d point sets.\n',num_iterations);
-    fprintf(1,'Instructions: \n');
-    fprintf(1,'Left click on the plot to create points. \n');
-    fprintf(1,'Right click on the plot to remove points \n');
-    fprintf(1,'Double click on the plot to end the set creation. \n');
-    fprintf(1,'When the last set is completed, another plot will be created to show results. \n');
-    
-    
-    % Initialize the paths_array
-    clear point_array
-    point_array{num_iterations} = [0 0];
-    for i_set = 1:num_iterations
-        
-        % Set the title header
-        UserData.title_header = sprintf('Set %.0d of %.0d',i_set,num_iterations);
-        
-        % Save the results
-        set(gcf,'UserData',UserData);
-        
-        pointsXY = fcn_Points_fillPointSetViaUserInputs(fig_num);
-        point_array{i_set} = pointsXY;
-    end
-    
-    %% Plot the results
-    clear data;
-    
-    % Plot the results
-    fig_num = 13;
-    fcn_Points_plotSetsXY(point_array);
-    %fcn_Points_plotSetsXY(point_array,fig_num);
-    
+num_iterations = input('How many point sets do you want to create? [Hit enter for default of 3]:','s');
+if isempty(num_iterations)
+    num_iterations = 3;
+else
+    num_iterations = str2double(num_iterations);
 end
+fprintf(1,'\n Filling in %.0d point sets.\n',num_iterations);
+fprintf(1,'Instructions: \n');
+fprintf(1,'Left click on the plot to create points. \n');
+fprintf(1,'Right click on the plot to remove points \n');
+fprintf(1,'Double click on the plot to end the set creation. \n');
+fprintf(1,'When the last set is completed, another plot will be created to show results. \n');
+
+    
+% Initialize the paths_array
+clear point_array
+point_array{num_iterations} = [0 0];
+for i_set = 1:num_iterations
+    
+    % Set the title header
+    UserData.title_header = sprintf('Set %.0d of %.0d',i_set,num_iterations);
+    
+    % Save the results
+    set(gcf,'UserData',UserData);
+    
+    pointsXY = fcn_Points_fillPointSetViaUserInputs(fig_num);
+    point_array{i_set} = pointsXY;
+end
+
+clear data, close;
+% Plot the results
+fig_num = 13;
+fcn_Points_plotSetsXY(point_array,fig_num);
+
+    
