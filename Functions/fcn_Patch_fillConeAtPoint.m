40f7473588794f29673abbcf5b354d9cf23f670d
JustinK21 <jkerr6372@gmail.com>
2022-08-08 11:50:32 -0400

    FEAT: Change base of Cone to a Square

--- Functions/fcn_Patch_fillConeAtPoint.m
+++ Functions/fcn_Patch_fillConeAtPoint.m
@@ -77,48 +77,43 @@ end
 %% create new cell structure for barrel information
 conePatch = struct('id',{},'color',{},'primitive',{},'primparams',{},'aabb',{},'pointsX',{},'pointsY',{});
 
+white = [1 1 1];
+orange = [1 .65 0]
+black = [0 0 0]
+
 for i = 1:size(xylocation,1)
-    conePatch(i).color = [1 .65 0];
-    conePatch(i+size(xylocation,1)).color = [1 1 1];
-    conePatch(i+2*size(xylocation,1)).color = [1 .65 0];
-    conePatch(i+3*size(xylocation,1)).color = [1 1 1];
-    conePatch(i+4*size(xylocation,1)).color = [1 .65 0];
-    conePatch(i+5*size(xylocation,1)).color = [0 0 0];
+    conePatch(i).color = orange;
+    conePatch(i+size(xylocation,1)).color = white;
+    conePatch(i+2*size(xylocation,1)).color = orange;
+    conePatch(i+3*size(xylocation,1)).color = white;
+    conePatch(i+4*size(xylocation,1)).color = orange;
+    conePatch(i+5*size(xylocation,1)).color = black;
     r = 0.18;
+    s = r*2
     
-    p1_x = (-r/(sqrt(2)+1));
-    p1_y = -r;
-    
-    p2_x = -r;
-    p2_y = (-r/(sqrt(2)+1));
-    
-    p3_x = -r;
-    p3_y = (r/(sqrt(2)+1));
-    
-    p4_x = (-r/(sqrt(2)+1));
-    p4_y = r;
-    
-    p5_x = (r/(sqrt(2)+1));
-    p5_y = r;
-    
-    p6_x = r;
-    p6_y = (r/(sqrt(2)+1));
-    
-    p7_x = r;
-    p7_y = -(r/(sqrt(2)+1));
-    
-    p8_x = (r/(sqrt(2)+1));
-    p8_y = -r;
+      p1_x = -s/2;
+    p1_y = s/2;
+
+    p2_x = -s/2;
+    p2_y = -s/2
+
+    p3_x = s/2;
+    p3_y = -s/2
+
+    p4_x = s/2;
+    p4_y = s/2;
     
     t = linspace(0,2*pi);
     xycirclepoints = [cos(t);sin(t)];
     
     rot_mat = [cos(orientation(i)) -sin(orientation(i)); sin(orientation(i)) cos(orientation(i))];
     
-    xypoints = rot_mat*[p1_x p2_x p3_x p4_x p5_x p6_x p7_x p8_x; p1_y p2_y p3_y p4_y p5_y p6_y p7_y p8_y];
+    xypoints = rot_mat*[p1_x p2_x p3_x p4_x; p1_y p2_y p3_y p4_y];
     
     conePatch(i).pointsX = [xypoints(1,:)]' + xylocation(i,1);
     conePatch(i).pointsY = [xypoints(2,:)]' + xylocation(i,2);
+
+
     conePatch(i+size(xylocation,1)).pointsX = [(r-.03)*xycirclepoints(1,:)]' + xylocation(i,1);
     conePatch(i+size(xylocation,1)).pointsY = [(r-.03)*xycirclepoints(2,:)]' + xylocation(i,2);
     conePatch(i+2*size(xylocation,1)).pointsX = [(r-.06)*xycirclepoints(1,:)]' + xylocation(i,1);
